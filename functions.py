f5753e68b06851aafeee3116881a222054580b7e
N4ch0ssss <ksiazek@student.agh.edu.pl>
2026-04-22 18:51:05 +0200

    Szymek branch (#11)
    * fix: poprawka load_audio dla przeplatanych kanalow stereo
    
    * add: folder DIR z nagraniami oraz notebook
    
    * usuniete komentarze, rozdzielenie funkcji oraz poprawienie wygladu

--- functions.py
+++ functions.py
@@ -4,12 +4,14 @@ import os
 import glob
 from scipy import fft
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def load_audio(file_path):
-    """Load audio from any format (m4a, wav, mp3, etc.) using PyAV."""
+    ''' There is a space for future description of the function '''
     container = av.open(file_path)
     audio_stream = next(s for s in container.streams if s.type == "audio")
     sr = audio_stream.sample_rate
+    channels = audio_stream.channels  
 
     frames = []
     for frame in container.decode(audio_stream):
@@ -17,21 +19,28 @@ def load_audio(file_path):
 
     container.close()
 
-    # Flatten, convert to float32, and mix down to mono if stereo
-    audio = np.concatenate(frames, axis=1).mean(axis=0).astype(np.float32)
+    audio = np.concatenate(frames, axis=1)[0]  
+    
+    if channels == 2:
+        audio = audio[::2] 
 
-    # Normalize to -1.0 / +1.0 range (same as librosa)
+    audio = audio.astype(np.float32)
     if audio.max() > 1.0:
         audio = audio / np.iinfo(np.int16).max
 
     return audio, sr
 
-def load_all(directory, extension="*.m4a"):
-    """Load all .m4a files from a given directory. Returns a list of (filename, y, sr) tuples."""
-    files = sorted(glob.glob(os.path.join(directory, extension)))
+def load_all(directory):
+    
+    """Load all audio files (.m4a, .wav, .mp3) from a given directory."""
+    files = []
+    for ext in ("*.m4a", "*.wav", "*.mp3"):
+        files.extend(glob.glob(os.path.join(directory, ext)))
+        
+    files = sorted(files)
 
     if not files:
-        print(f"No .m4a files found in: {directory}")
+        print(f"No audio files found in: {directory}")
         return []
 
     recordings = []
@@ -42,23 +51,48 @@ def load_all(directory, extension="*.m4a"):
         print(f"Loaded: {filename} | {sr} Hz | {len(y)/sr:.2f}s")
 
     return recordings
-
-def dominant_frequencies(recordings, ml=20):
+def dominant_frequencies(recordings, start_ml=20, step_ml=10, min_freq=0, max_freq=3000):
+    ''' There is a space for future description of the function '''
     frequencies = []
     volume = []
-    ml = 20
+    beer_names = []
+    fft_data = []
+    ml = start_ml
 
-    for filename, y, sr in recordings:  # no axes needed
+    for filename, y, sr in recordings:
         fft_result = np.abs(fft.rfft(y))
         freqs = fft.rfftfreq(len(y), 1 / sr)
-        dominant_freq = freqs[np.argmax(fft_result)]
+
+        valid_indices = np.where((freqs > min_freq) & (freqs < max_freq))[0]
+        best_index_in_valid = np.argmax(fft_result[valid_indices])
+        actual_best_index = valid_indices[best_index_in_valid]
+        dominant_freq = freqs[actual_best_index]
+
         frequencies.append(dominant_freq)
         volume.append(ml)
-        ml += 20
+        beer_names.append(filename.split('-')[0])
+        fft_data.append((filename, freqs, fft_result))
+        ml += step_ml
 
-    data = {'frequencies': frequencies, 'volume': volume}
-    df = pd.DataFrame(data)
-
-    return df
+    return pd.DataFrame({'Beer': beer_names, 'frequencies': frequencies, 'volume': volume}), fft_data
 
 
+def plot_spectrum(fft_data, df, max_freq=3000):
+    ''' There is a space for future description of the function '''
+    beer_names = df['Beer'].unique()
+
+    for beer in beer_names:
+        plt.figure(figsize=(10, 5))
+        beer_df = df[df['Beer'] == beer]
+        beer_fft = [(filename, freqs, fft_result) for (filename, freqs, fft_result) in fft_data if filename.split('-')[0] == beer]
+
+        for (filename, freqs, fft_result), (_, row) in zip(beer_fft, beer_df.iterrows()):
+            plot_mask = freqs < max_freq
+            plt.plot(freqs[plot_mask], fft_result[plot_mask], alpha=0.7, label=f"{row['volume']}ml")
+
+
+        plt.title(f"Frequency Spectrum - {beer}")
+        plt.xlabel("Częstotliwość [Hz]")
+        plt.ylabel("Amplituda")
+        plt.legend(loc='upper right', fontsize='small')
+        plt.show()
