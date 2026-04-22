8b9ad9114abb73332c937e23d8145b8e42235e87
N4ch0ssss <ksiazek@student.agh.edu.pl>
2026-04-22 15:04:52 +0200

    usuniete komentarze, rozdzielenie funkcji oraz poprawienie wygladu

--- functions.py
+++ functions.py
@@ -7,10 +7,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def load_audio(file_path):
+    ''' There is a space for future description of the function '''
     container = av.open(file_path)
     audio_stream = next(s for s in container.streams if s.type == "audio")
     sr = audio_stream.sample_rate
-    channels = audio_stream.channels  # <-- pobierz liczbę kanałów
+    channels = audio_stream.channels  
 
     frames = []
     for frame in container.decode(audio_stream):
@@ -18,11 +19,10 @@ def load_audio(file_path):
 
     container.close()
 
-    audio = np.concatenate(frames, axis=1)[0]  # shape: (1, N) -> (N,)
+    audio = np.concatenate(frames, axis=1)[0]  
     
-    # Przeplatane stereo: [L, R, L, R, ...] -> weź tylko lewy kanał
     if channels == 2:
-        audio = audio[::2]  # co druga próbka = lewy kanał
+        audio = audio[::2] 
 
     audio = audio.astype(np.float32)
     if audio.max() > 1.0:
@@ -31,9 +31,9 @@ def load_audio(file_path):
     return audio, sr
 
 def load_all(directory):
+    
     """Load all audio files (.m4a, .wav, .mp3) from a given directory."""
     files = []
-    # Szukamy różnych formatów w podanym folderze
     for ext in ("*.m4a", "*.wav", "*.mp3"):
         files.extend(glob.glob(os.path.join(directory, ext)))
         
@@ -51,81 +51,48 @@ def load_all(directory):
         print(f"Loaded: {filename} | {sr} Hz | {len(y)/sr:.2f}s")
 
     return recordings
-
 def dominant_frequencies(recordings, start_ml=20, step_ml=10, min_freq=0, max_freq=3000):
-    frequencies = []
-    volume = []
-    beer_names = []  # Nowa lista na nazwy piw
-    ml = start_ml
-
-    for filename, y, sr in recordings:  # no axes needed
-        fft_result = np.abs(fft.rfft(y))
-        freqs = fft.rfftfreq(len(y), 1 / sr)
-        # --- ZMIANA: Tworzymy "okno" od 800 Hz do 3000 Hz ---
-        # Znak & oznacza "i jednocześnie"
-        valid_indices = np.where((freqs > min_freq) & (freqs < max_freq))[0]
-        
-        # Szukamy najgłośniejszego piku tylko w tym bezpiecznym przedziale
-        best_index_in_valid = np.argmax(fft_result[valid_indices])
-        actual_best_index = valid_indices[best_index_in_valid]
-        
-        dominant_freq = freqs[actual_best_index]
-
-        # dominant_freq = freqs[np.argmax(fft_result)]
-        frequencies.append(dominant_freq)
-        volume.append(ml)
-
-        # Wyciąganie nazwy piwa z nazwy pliku 
-        # (dzieli np. "Kasztelan-20.m4a" na ["Kasztelan", "20.m4a"] i bierze pierwszy element)
-        beer_name = filename.split('-')[0]
-        beer_names.append(beer_name)
-
-        ml += step_ml
-
-    data = {'Beer': beer_names,'frequencies': frequencies, 'volume': volume}
-    df = pd.DataFrame(data)
-
-    return df
-
-# Jeśli nadal potrzebujesz DataFrame z wynikami, ale chcesz też wykres:
-def dominant_frequencies_with_plot(recordings, start_ml=20, step_ml=10, min_freq=0, max_freq=3000):
+    ''' There is a space for future description of the function '''
     frequencies = []
     volume = []
     beer_names = []
+    fft_data = []
     ml = start_ml
 
-    # Tworzymy figurę przed pętlą
-    plt.figure(figsize=(10, 5))
-
     for filename, y, sr in recordings:
-        # FFT
         fft_result = np.abs(fft.rfft(y))
         freqs = fft.rfftfreq(len(y), 1 / sr)
-        
-        print(f"Sample rate: {sr}, długość y: {len(y)}, shape: {y.shape}")
 
-        # Logika szukania dominanty (z Twojego kodu)
         valid_indices = np.where((freqs > min_freq) & (freqs < max_freq))[0]
         best_index_in_valid = np.argmax(fft_result[valid_indices])
         actual_best_index = valid_indices[best_index_in_valid]
         dominant_freq = freqs[actual_best_index]
 
-        # RYSOWANIE: Dodajemy linię dla tego konkretnego nagrania
-        # Ograniczamy zakres x do max_freq, żeby wykres nie był "pusty" na końcu
-        plot_mask = freqs < max_freq 
-        plt.plot(freqs[plot_mask], fft_result[plot_mask], alpha=0.7, label=f"{ml}ml - {filename}")
-        
-        # Zbieranie danych do DF
         frequencies.append(dominant_freq)
         volume.append(ml)
-        beer_name = filename.split('-')[0]
-        beer_names.append(beer_name)
+        beer_names.append(filename.split('-')[0])
+        fft_data.append((filename, freqs, fft_result))
         ml += step_ml
 
-    plt.title("Analiza Spektralna - Amplituda od Częstotliwości")
-    plt.xlabel("Częstotliwość [Hz]")
-    plt.ylabel("Amplituda")
-    plt.legend(loc='upper right', fontsize='small')
-    plt.show()
+    return pd.DataFrame({'Beer': beer_names, 'frequencies': frequencies, 'volume': volume}), fft_data
 
-    return pd.DataFrame({'Beer': beer_names, 'frequencies': frequencies, 'volume': volume})
+
+def plot_spectrum(fft_data, df, max_freq=3000):
+    ''' There is a space for future description of the function '''
+    beer_names = df['Beer'].unique()
+
+    for beer in beer_names:
+        plt.figure(figsize=(10, 5))
+        beer_df = df[df['Beer'] == beer]
+        beer_fft = [(filename, freqs, fft_result) for (filename, freqs, fft_result) in fft_data if filename.split('-')[0] == beer]
+
+        for (filename, freqs, fft_result), (_, row) in zip(beer_fft, beer_df.iterrows()):
+            plot_mask = freqs < max_freq
+            plt.plot(freqs[plot_mask], fft_result[plot_mask], alpha=0.7, label=f"{row['volume']}ml")
+
+
+        plt.title(f"Frequency Spectrum - {beer}")
+        plt.xlabel("Częstotliwość [Hz]")
+        plt.ylabel("Amplituda")
+        plt.legend(loc='upper right', fontsize='small')
+        plt.show()
