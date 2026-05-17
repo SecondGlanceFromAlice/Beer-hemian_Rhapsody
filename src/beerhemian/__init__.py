0ec1ff5b57679c5143458a8d94805da7622775e3
Alicja <ajagielska@com-4t.pl>
2026-05-18 00:13:26 +0200

    Forgot to add before (#40)
    And now code doesn't want to work (hope after this change it will work)

--- src/beerhemian/__init__.py
+++ src/beerhemian/__init__.py
@@ -1,2 +1,2 @@
-from .functions import load_audio, load_all, dominant_frequencies, plot_spectrum, plot_with_curve
+from .functions import load_audio, load_all, dominant_frequencies, plot_spectrum, plot_spectrym_multiple, plot_with_curve
 from .fake_waves import sine_wave
